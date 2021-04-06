3010fa2eb4652548410797e724dc680fbc2614d4
Debashis876 <81948347+Debashis876@users.noreply.github.com>
2021-04-06 23:59:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['Clump Thickness', 'Uniformity of Cell Size','Uniformity of Cell Shape','Marginal Adhesion','Single Epithelial Cell Size','Bare Nuclei','Bland Chromatin','Normal Nucleoli','Mitoses']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    prediction = model.predict(df)   
+    if prediction == 1:
+        res_val = " breast cancer "
+    else:
+        res_val = "no breast cancer"
+    return render_template('index.html', prediction_text='Patient has  {}'.format(res_val))
+if __name__ == "__main__":
+    app.run(debug = True)
